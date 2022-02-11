fa2968f5ce0a6c0a24d194c9c168a3b1d54d1f20
Davi Marques <davimarquesgar@gmail.com>
2022-02-11 11:29:52 -0300

    Canvas direto pelo HTML

--- gameBabel.js
+++ gameBabel.js
@@ -3,7 +3,6 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 512;
 canvas.height = 480;
-document.body.appendChild(canvas);
 
 
 // Imagem de fundo
